816d2e3436700f925c95fd17f039ed3d53bc6192
roberseik <roberixion@gmail.com>
2016-04-14 12:18:50 +0200

    semi_entrega

--- Practica-1/Practica1_Name.pl
+++ Practica-1/Practica1_Name.pl
@@ -1,23 +1,50 @@
 :-module(_,_).
 
-% Funciones Adicionales
+%---------------------------------------------------------------
+% Implementaciones basicas para la realizacion de los predicados
+%---------------------------------------------------------------
+
+% IMPLEMENTACIONES PARA PARTE 1:
+%-------------------------------
+
+% my_append/2
+% Concatena 2 listas, dejando el resultado en una lista.
+my_append([],X,X).
+my_append([X|Y],Z,[X|R]):- my_append(Y,Z,R).
+
+
+
+% IMPLEMENTACIONES PARA PARTE 2:
+%-------------------------------
+% nat/1
+% Verifica que los numeros son naturales.
 nat(0).
 nat(s(X)):-nat(X).
 
+% plus/3
+% Suma los elementos X e Y, dejando el resultado en Z.
 plus(0,X,X):-nat(X).
 plus(s(X),Y,s(Z)):- plus(X,Y,Z).
 
+% less/2
+% Comprueba que X es menor que Y
 less(0,s(X)):- nat(X).
 less(s(X),s(Y)) :- less(X,Y).
 
+% mod/3
+% Realiza el modulo de X e Y, dejando el resultado en Z.
 mod(X,Y,X) :- less(X,Y).
 mod(X,Y,Z) :- plus(X1,Y,X), mod(X1,Y,Z).
  
+% times/3
+% Multiplica los elementos X e Y, dejando el resultado en Z.
 times(X,0,0):-nat(X).
 times(X,s(Y),Z):- times(X,Y,Acc), plus(Acc,X,Z).
 
-numpar(N,Acc,R):- mod(N,s(s(0)),0), plus(Acc,N,R).
-numpar(N,Acc,R):- mod(N,s(s(0)),s(0)),plus(0,Acc,R). 
+
+%--------------------------------------------------------------
+% Parte 2: arbolBalanceadoPar y arbolAmplificado
+%-------------------------------------------------------------
 
 % arbolBalanceadoPar/1 
 arbolBalanceadoPar(void).
@@ -53,4 +80,3 @@ subtree(tree(par(S,N),L,R),Root,STree):-
 	subtree(R,Root,R1),
 	times(N,Root,Res),
 	STree = tree(par(S,Res),L1,R1).
-
