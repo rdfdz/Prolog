1d2bd1c7d4d43321cd2e7be187cade22791f7646
roberseik <roberixion@gmail.com>
2016-05-18 01:36:46 +0200

    inicios

--- Practica-2/grafo.pl
+++ Practica-2/grafo.pl
@@ -1,15 +1,20 @@
 :-module(_,_).
 
 :-dynamic nodo/2.
+:-dynamic arco/2.
 
-enumerar(Arco,EnumNodos,_):-
+% contemplar fallos ?
+enumerar(Arco,EnumNodos,EnumArcos):-
 	dbNodo(Arco),!,
-	findall(elem(X,Y),nodo(X,Y),EnumNodos).
+	findall(elem(K,V),nodo(K,V),EnumNodos),
+	findall(elem(K,V),arco(K,V),EnumArcos).
 	
 dbNodo([]).
 dbNodo([X-Y|Z]):-
-	hashmap(nodo(X,1)),
-	hashmap(nodo(Y,1)),
+	hashmap(nodo(X,1)), %% No es aleatorio
+	hashmap(nodo(Y,2)), %% fallo inserta repetido
+	absoluto(X,Y,K),
+	hashmap(arco(X-Y,K)),
 	dbNodo(Z).
 
 hashmap(Element):-
@@ -17,22 +22,9 @@ hashmap(Element):-
 	assert(Element).
 hashmap(_).
 
+absoluto(X,Y,Z):- nodo(X,V1),nodo(Y,V2), Z is abs(V1-V2).
 
 
-
-delete(V):-retract(nodo(V,_)).
-limpiar(X,Y):-retractall(nodo(X,Y)).
-
-
-nodos([],[]).
-nodos([X-Y|Z],L):-
-	append([X,Y],Acc,L),
-	nodos(Z,Acc).
-
-asignacion(L,_):-
-	nodos(L,N),
-	sort(N,X),
-	write(X).
 
 
 
