35851ea294fe3f26d02870150409cfa348e8ba82
roberseik <roberixion@gmail.com>
2016-06-01 19:20:29 +0200

    semifinal

--- Practica-2/mejora.pl
+++ Practica-2/mejora.pl
@@ -6,47 +6,46 @@
 :-dynamic pareja/3.
 
 
-immerse([buzo(B1,T1),buzo(B2,T2)|R],L) :- 
+immerse([buzo(B1,T1),buzo(B2,T2)|R],L,Length) :- 
 	T1>T2,
-	assert(pareja(B1,B2,T2)),
+	hashmap(B1,B2,T2,[buzo(B1,T1),buzo(B2,T2)|R],L,Length),
 	TR is abs(T1 -T2),
-	immerse([buzo(B1,TR)|R],L),!.
-immerse([buzo(B1,T1),buzo(B2,T2)|R],L) :- 
+	immerse([buzo(B1,TR)|R],L,Length),!.
+immerse([buzo(B1,T1),buzo(B2,T2)|R],L,Length) :- 
 	T1<T2,
-	assert(pareja(B1,B2,T1)),
+	hashmap(B1,B2,T1,[buzo(B1,T1),buzo(B2,T2)|R],L,Length),
 	TR is abs(T1 -T2),
-	immerse([buzo(B2,TR)|R],L),!.
-immerse([buzo(B1,T1),buzo(B2,T2)|R],L) :- 
+	immerse([buzo(B2,TR)|R],L,Length),!.
+immerse([buzo(B1,T1),buzo(B2,T2)|R],L,Length) :- 
 	T1=T2,
-	assert(pareja(B1,B2,T1)),
-	immerse(R,L),!.
-immerse(L,List):-  
-%	findall(X,pareja(X,garcia,Z),L1),
-%	findall(Y,pareja(garcia,Y,Z),L2),
-	write(L),nl,write(List),!.
+	hashmap(B1,B2,T1,[buzo(B1,T1),buzo(B2,T2)|R],L,Length),
+	immerse(R,L,Length),!.
+immerse(_,_,Length):- 
+	findall(pareja(X,Y,Z),pareja(X,Y,Z),L),
+	length(L,R),
+	R =:= Length,!.
+immerse([buzo(B1,T1)],List,Length):-
+	del(buzo(B1,_),List,Del),
+	append([buzo(B1,T1)],Del,NewList),
+	moveLast(Del,Move),
+	immerse(NewList,Move,Length),!.
+immerse([],List,Length):-
+	moveLast(List,Move),
+	immerse(List,Move,Length),!.
 
-prueba([buzo(B,T)],List,FIN):-
-	findall(X,pareja(X,garcia,Z),L1),
-	findall(Y,pareja(garcia,Y,Z),L2),
-	append([L1],L2,FIN).
+del(buzo(B,_),[buzo(B,_)|Tail],Tail):-!.
+del(buzo(B1,_),[buzo(B2,T2)|Tail],[buzo(B2,T2)|Tail1]):-
+	del(buzo(B1,_),Tail,Tail1).
 
-exist(Element,[buzo(B,_)|R],R):- Element = B.
-exist(Element,[buzo(B,_)|R],[buzo(B,_)|R1]):-
-	exist(Element,R,R1).
+hashmap(B1,B2,T,_,_,_):-
+	\+(pareja(B1,B2,_)),
+        \+(pareja(B2,B1,_)),
+	assert(pareja(B1,B2,T)).
+hashmap(_,_,_,[buzo(B1,T1),buzo(_,_)|R],L,Length):-
+	immerse([buzo(B1,T1)|R],L,Length).
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+moveLast([], []):-!.
+moveLast(L, [H|T]) :- append(T, [H], L).
 
 permutation(Bs, [A|As]):-
 	append(Xs, [A|Ys], Bs), 
@@ -60,12 +59,21 @@ timecheck(_,_):- retractall(pareja(_,_,_)), fail.
 sumlist([], 0).
 sumlist([X|Xs], S):- sumlist(Xs, S2), S is S2 + X.
 
+numlist(X, X, [X]).
+numlist(Von, Bis, [Von | Result]):-
+	Von =< Bis, Von1 is Von+1,
+	numlist(Von1, Bis, Result).
+
 reparacion(Equipo,Tiempo,OrdenParejas):-
 	permutation(Equipo,Backtraking),
-	immerse(Backtraking,Backtraking),
+	length(Equipo,Len),
+	Leng is Len - 1,
+	numlist(0,Leng,NumPareja),
+	sumlist(NumPareja,LenPareja),
+	immerse(Backtraking,Backtraking,LenPareja),
 	findall(T,pareja(_,_,T), LT),
 	sumlist(LT,Total), %write(LT),write(Sum),nl,
 	timecheck(Total,Tiempo),
 	findall(pareja(X,Y,Z),pareja(X,Y,Z),OrdenParejas).
-	%retractall(pareja(_,_,_)).
+
 
