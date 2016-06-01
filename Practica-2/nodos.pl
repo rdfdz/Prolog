f7034a60e3ab507c4abcb04a3ea09cd1bf23f42e
MJ29 <maria_jose290594@hotmail.com>
2016-06-01 22:26:03 +0200

    comentarios

--- Practica-2/nodos.pl
+++ Practica-2/nodos.pl
@@ -1,18 +1,24 @@
+% --------------------------------------------------------------
+% PRACTICA 2: PROGRAMACION DECLARATIVA
+%
+% autores:
+% Roberto Daniel Fernandez Castro
+% Maria Jose Alobuela Collaguazo
+% Gerald Renzo Martinez Alvaro
+%---------------------------------------------------------------
+
+%--------------------------------------------------------------
+% PARTE 1: PROBLEMA DE ENUMERACION
+%--------------------------------------------------------------
+
 :-module(_,_).
 
+%Predicados dinamicos necesarios para añadir clausulas a la base de conocimientos
 :-dynamic nodo/2.
 :-dynamic arco/2.
 
-enumerar(Arco,EnumNodos,EnumArcos):-
-	length(Arco,Long),
-	findall(X,between(1,Long+1,X),Xs),
-	position_node(Xs,[],L),
-	database(Arco,L),!,
-	findall(elem(K,V),nodo(K,V),EnumNodos),
-	findall(elem(K,V),arco(K,V),EnumArcos),
-	retractall(nodo(_,_)),
-	retractall(arco(_,_)).
-	
+%database/2
+%Recorre la lista de las aristas del grafo y la lista de la secuencia de numeros para unir cada nodo o arco con su respectivo numero.
 database([],_).
 database([X-Y|Z],L):-
 	hashmap(X,L,L1), 
@@ -21,14 +27,20 @@ database([X-Y|Z],L):-
 	assert(arco(X-Y,V)),
 	database(Z,L2).
 
+%hashmap/3
+%inserta en la base de hechos cada uno de los nodos del grafo.
 hashmap(K,[V|T],L):-
 	\+(nodo(K,_)),        
 	assert(nodo(K,V)),
 	L = T.
 hashmap(_,L,L).
 
+%absolute/3
+%calcula la diferencia en valor absoluto de V1-V2 para asignar dicho numero a la arista.
 absolute(X,Y,Z):- nodo(X,V1),nodo(Y,V2), Z is abs(V1-V2).
 
+%position_node/3
+%Devuelve una lista con la secuencia adecuada para enumerar a los nodos del grafo
 position_node([],Acc,Acc).
 position_node([First|Y],Acc,L):-
 	last([First|Y],Last),
@@ -40,3 +52,14 @@ position_node([First|Y],Acc,L):-
 	append(Acc,[First],L),
 	position_node(Y,L,L).
 	
+%enumerar/3
+%Dada una lista con las aristas de un grafo conexo, se enumeran sus nodos y arcos con sus correspondientes numeros. 
+enumerar(Arco,EnumNodos,EnumArcos):-
+	length(Arco,Long),
+	findall(X,between(1,Long+1,X),Xs),
+	position_node(Xs,[],L),
+	database(Arco,L),!,
+	findall(enum(V,K),nodo(K,V),EnumNodos),
+	findall(enum(V,K),arco(K,V),EnumArcos),
+	retractall(nodo(_,_)),
+	retractall(arco(_,_)).
